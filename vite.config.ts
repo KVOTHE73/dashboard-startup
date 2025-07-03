d0fd52e3e2474ba0019162cf9063cb49ee26c059
KVOTHE73 <syn231000@hotmail.com>
2025-07-03 19:48:51 +0200

    Update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/dashboard-startup/" : "/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
@@ -13,6 +14,6 @@ export default defineConfig({
     },
   },
   optimizeDeps: {
-    exclude: ['vue-demi']
-	}
-});
+    exclude: ["vue-demi"],
+  },
+}));
